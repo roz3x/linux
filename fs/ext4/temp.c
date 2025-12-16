132f1daacd3efd7ff93438bc0ff925a518a79c8b
Shivang Upadhyay <shivangu@linux.ibm.com>
2025-12-16 14:04:51 +0530

    fix node alloction size.

--- fs/ext4/temp.c
+++ fs/ext4/temp.c
@@ -7,7 +7,7 @@
 struct ext4_range_node {
 	struct rb_node rb;
 
-	int inode;
+	u64 inode;
 	u64 offset;
 	enum {BOUNDRY_START, BOUNDRY_END} value;
 	int refs;
@@ -18,9 +18,9 @@ struct ext4_range_node {
 static atomic_t tree_size = ATOMIC_INIT(0);
 
 static struct rb_root root = RB_ROOT;
-static struct mutex lock;
+static DEFINE_MUTEX(lock);
 
-static struct ext4_range_node* find_first_le(int inode, u64 offset) {
+static struct ext4_range_node* find_first_le(u64 inode, u64 offset) {
 
 	struct rb_node **new = &root.rb_node;
 	struct rb_node *parent = NULL;
@@ -55,8 +55,10 @@ static struct ext4_range_node* find_first_le(int inode, u64 offset) {
 static void insert_exact(struct ext4_range_node *new_node){
 	struct rb_node **new = &root.rb_node;
 	struct rb_node *parent = NULL;
-	unsigned int long long offset = new_node->offset;
-	unsigned int long long inode = new_node->inode;
+	u64 offset = new_node->offset;
+	u64 inode = new_node->inode;
+
+	printk("[shivang] inserting %llx\n", offset);
 
 	while (*new) {
 		struct ext4_range_node *this = rb_entry(*new, struct ext4_range_node, rb);
@@ -85,7 +87,7 @@ static void insert_exact(struct ext4_range_node *new_node){
 	atomic_inc(&tree_size);
 
 }
-static void delete_exact(int inode, u64 offset) {
+static void delete_exact(u64 inode, u64 offset) {
 
 	struct rb_node **new = &root.rb_node;
 	struct rb_node *parent = NULL;
@@ -116,19 +118,21 @@ static void delete_exact(int inode, u64 offset) {
 	return;
 }
 
-void ext4_completion_cb(int inode, u64 start_offset, u64 end_offset) {
+void ext4_completion_cb(u64 inode, u64 start_offset, u64 end_offset) {
 
 	mutex_lock(&lock);
+	printk("[shivang] delete request :  %llx %llx\n", start_offset, end_offset);
 
 	printk("[shivang] tree_size : %d\n", atomic_read(&tree_size));
 	delete_exact(inode, start_offset);
 	printk("[shivang] tree_size : %d\n", atomic_read(&tree_size));
-	delete_exact(inode, start_offset);
+	delete_exact(inode, end_offset);
 
 	mutex_unlock(&lock);
 }
 
-void ext4_insert_or_wait(int inode, u64 start_offset,
+
+void ext4_insert_or_wait(u64 inode, u64 start_offset,
 	u64 end_offset) {
 	struct ext4_range_node *node_start, *node_end,
 		*best_start, *best_end;
@@ -141,10 +145,10 @@ void ext4_insert_or_wait(int inode, u64 start_offset,
 	 * need to answer question, how much DIO's can be run at same time on a disk?
 	 */
 
-	printk("[shivang] tree_size : %d\n", atomic_read(&tree_size));
+	printk("[shivang] tree_size : %d %llx %llx\n", atomic_read(&tree_size), start_offset, end_offset);
 
-	node_start = (struct ext4_range_node*) kmalloc(sizeof(struct ext4_range_node*), GFP_KERNEL);
-	node_end = (struct ext4_range_node*) kmalloc(sizeof(struct ext4_range_node*), GFP_KERNEL);
+	node_start = (struct ext4_range_node*) kmalloc(sizeof(struct ext4_range_node), GFP_KERNEL);
+	node_end = (struct ext4_range_node*) kmalloc(sizeof(struct ext4_range_node), GFP_KERNEL);
 
 	*node_start = (struct ext4_range_node) {
 		.refs = 0,
@@ -169,11 +173,12 @@ void ext4_insert_or_wait(int inode, u64 start_offset,
 		best_start = find_first_le(inode, node_start->offset);
 		best_end = find_first_le(inode, node_end->offset);
 
-		mutex_unlock(&lock);
+		// mutex_unlock(&lock);
 
 		if (best_start != NULL && best_start->inode == node_start->inode
 			&& (best_start->offset == node_start->offset || best_start->value == BOUNDRY_START)) {
 			/* we wait for best_start's lock */
+			mutex_unlock(&lock);
 			schedule();
 			continue;
 		}
@@ -181,12 +186,13 @@ void ext4_insert_or_wait(int inode, u64 start_offset,
 		if (best_end != NULL && best_end->inode == node_end->inode
 			&& (best_end->value == BOUNDRY_START || best_end->offset >= node_start->offset )) {
 			/* we wait for best_end's lock */
+			mutex_unlock(&lock);
 			schedule();
 			continue;
 		}
 
 		/* there is no overlap after this point */
-		mutex_lock(&lock);
+		// mutex_lock(&lock);
 		insert_exact(node_start);
 		insert_exact(node_end);
 		mutex_unlock(&lock);
@@ -196,5 +202,3 @@ void ext4_insert_or_wait(int inode, u64 start_offset,
 
 	return ;
 }
-
-
